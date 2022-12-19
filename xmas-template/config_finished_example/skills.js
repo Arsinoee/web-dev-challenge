266e8f87e1f5c4fc3f40753d7a7d22c9d2ccdc0b
Arsinoee <116231263+Arsinoee@users.noreply.github.com>
2022-12-18 21:18:16 -0700

    Create skills.js

---
+++ xmas-template/config_finished_example/skills.js
@@ -0,0 +1,84 @@
+const userSkills = {
+    "skills": [
+        {
+            "name": "Python",
+            "icon": "fab fa-python",
+            "link": "https://www.python.org"
+        },
+        {
+            "name": "C (language)",
+            "icon": "fas fa-file-code",
+            "link": null,
+        },
+        {
+            "name": "JavaScript",
+            "icon": "fab fa-js",
+            "link": null,
+        },
+        {
+            "name": "Node",
+            "icon": "fab fa-node-js",
+            "link": "https://nodejs.org"
+        },
+        {
+            "name": "Express",
+            "icon": "fas fa-server",
+            "link": "https://expressjs.org"
+        },
+        {
+            "name": "Git",
+            "icon": "fab fa-git-alt",
+            "link": "https://git-scm.com"
+        },
+        {
+            "name": "MongoDB",
+            "icon": "fas fa-database",
+            "link": "https://mongodb.com"
+        },
+        {
+            "name": "JSON Web Tokens",
+            "icon": "fas fa-key",
+            "link": "https://jwt.io"
+        },
+        {
+            "name": "HTML",
+            "icon": "fab fa-html5",
+            "link": "https://html.spec.whatwg.org/multipage/"
+        },
+        {
+            "name": "CSS",
+            "icon": "fab fa-css3-alt",
+            "link": "https://www.w3.org/Style/CSS/"
+        },
+        {
+            "name": "Adobe Photoshop",
+            "icon": "fas fa-arrow-pointer",
+            "link": "https://adobe.com/photoshop"
+        },
+        {
+            "name": "Adobe Illustrator",
+            "icon": "fas fa-arrow-pointer",
+            "link": "https://adobe.com/illustrator"
+        },
+        {
+            "name": "Adobe InDesign",
+            "icon": "fas fa-arrow-pointer",
+            "link": "https://adobe.com/indesign"
+        },
+        {
+            "name": "Postman",
+            "icon": "fas fa-arrow-pointer",
+            "link": "https://postman.com"
+        },
+        "Great Communication Skills",
+        "Fast Learner",
+        "Team Player",
+        "Leadership Skills",
+        "Problem Solving Skills",
+        "Creative",
+        "Detail Oriented",
+        "Self Motivated",
+        "Self Starter",
+        "Hard Worker",
+    ]
+}
