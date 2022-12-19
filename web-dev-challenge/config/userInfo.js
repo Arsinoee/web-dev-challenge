15abfd663baa271de140e5d54d169976b0ca1f63
Arsinoee <116231263+Arsinoee@users.noreply.github.com>
2022-12-18 21:30:12 -0700

    Create userInfo.js

---
+++ web-dev-challenge/config/userInfo.js
@@ -0,0 +1,22 @@
+const userInfo = {
+    "image": "./image.png",
+    "headerFont": "Helvetica",
+    "name": {
+        "first": "Emily",
+        "last": "Helgeson"
+    },
+    "about": {
+        "title": "Title",
+        "description": "Dummy Bio goes here.",
+        "social": [
+            {
+                "name": "example-social-network",
+                "url": "http://example.com/social",
+                "icon": {
+                    "set": "fas",
+                    "iconName": "fa-info"
+                }
+            },
+        ]
+    }
+}
