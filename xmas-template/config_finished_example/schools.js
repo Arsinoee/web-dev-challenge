d5f7cfad8155bd700ffe97f86ad140b17ed3b620
Arsinoee <116231263+Arsinoee@users.noreply.github.com>
2022-12-18 21:18:15 -0700

    Create schools.js

---
+++ xmas-template/config_finished_example/schools.js
@@ -0,0 +1,53 @@
+const userSchools = {
+    "schools": [
+        {
+            "type": "Bachelor's Degree",
+            "name": "Dakota State University",
+            "address": {
+                "street": "820 N Washington Ave",
+                "city": "Madison",
+                "state": "SD",
+                "zip": "57042"
+            },
+            "major": "Cyber Operations",
+            "minor": "Computer Science",
+            "gpa": 3.5,
+            "graduation": "May 2026",
+            "clubs": [
+                {
+                    "name": "Emry Hall Council",
+                    "position": "President"
+                },
+                {
+                    "name": "Web Development Club",
+                    "position": "President"
+                },
+                {
+                    "name": "General Beadle Honors Program",
+                    "position": "Advisory Council Member"
+                },
+                {
+                    "name": "The Alliance at DSU",
+                    "position": "Member"
+                },
+                {
+                    "name": "Computer Club",
+                    "position": "Visitor"
+                },
+            ]
+        },
+        {
+            "type": "High School",
+            "name": "Hartington Public Schools",
+            "address": {
+                "street": "801 N Broadway Ave",
+                "city": "Hartington",
+                "state": "NE",
+                "zip": "68739"
+            },
+            "major": "General Studies",
+            "gpa": 3.5,
+            "graduation": "May 2022"
+        }
+    ]
+}
