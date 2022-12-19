b817e7547c97d57f8a31dab4f988937000f30069
Arsinoee <116231263+Arsinoee@users.noreply.github.com>
2022-12-18 21:18:18 -0700

    Create userInfo.js

---
+++ xmas-template/config_finished_example/userInfo.js
@@ -0,0 +1,78 @@
+const userInfo = {
+    "image": "./image.png",
+    "headerFont": "Courier New",
+    "name": {
+        "first": "Anden",
+        "last": "Wieseler"
+    },
+    "about": {
+        "title": "Graphic Designer/Web Developer/Software Developer",
+        "description": "I'm a young, ambitious, and creative individual who is passionate about design and technology. I have a strong background in graphic design and web development, and I am currently pursuing a degree in Cyber Operations. I am a quick learner and I am always looking for new opportunities to learn and grow.",
+        "social": [
+            {
+                "name": "github",
+                "url": "http://github.yltboi.me",
+                "icon": {
+                    "set": "fab",
+                    "iconName": "fa-github"
+                }
+            },
+            {
+                "name": "twitter",
+                "url": "http://twitter.yltboi.me",
+                "icon": {
+                    "set": "fab",
+                    "iconName": "fa-twitter"
+                }
+            },
+            {
+                "name": "facebook",
+                "url": "http://facebook.yltboi.me",
+                "icon": {
+                    "set": "fab",
+                    "iconName": "fa-facebook"
+                }
+            },
+            {
+                "name": "instagram",
+                "url": "http://instagram.yltboi.me",
+                "icon": {
+                    "set": "fab",
+                    "iconName": "fa-instagram"
+                }
+            },
+            {
+                "name": "snapchat",
+                "url": "http://snapchat.yltboi.me",
+                "icon": {
+                    "set": "fab",
+                    "iconName": "fa-snapchat"
+                }
+            },
+            {
+                "name": "email",
+                "url": "mailto:anden@yltboi.me",
+                "icon": {
+                    "set": "fas",
+                    "iconName": "fa-envelope"
+                }
+            },
+            {
+                "name": "linkedin",
+                "url": null,
+                "icon": {
+                    "set": "fab",
+                    "iconName": "fa-linkedin"
+                }
+            },
+            {
+                "name": "website",
+                "url": "https://www.anden.me",
+                "icon": {
+                    "set": "fas",
+                    "iconName": "fa-globe"
+                }
+            }
+        ]
+    }
+}
