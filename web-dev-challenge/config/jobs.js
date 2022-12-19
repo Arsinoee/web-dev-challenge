4e5ef0f5d19cd4987f12d43148db019bad6931ec
Arsinoee <116231263+Arsinoee@users.noreply.github.com>
2022-12-18 21:30:08 -0700

    Create jobs.js

---
+++ web-dev-challenge/config/jobs.js
@@ -0,0 +1,18 @@
+const userJobs = {
+    "jobs": [
+        {
+            "company": "South Dakota School of Mines",
+            "title": "Summer Student Worker",
+            "description": "I was responsible for rebuilding ranges and setting up the fourth floor of the Devereaux Library. Additionally, I worked to update asset records and warehouse labels.   ",
+            "start": "1 June 2022",
+            "end": "16 August 2022"
+        },
+        {
+            "company": "South Dakota School of Mines",
+            "title": "Facilities Intern",
+            "description": "In progress...",
+            "start": "15 December 2022",
+            "end": "16 August 2023"
+        },
+    ]
+}
