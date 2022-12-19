195799adc9fd6bebc9810aa904351866fda322d2
Arsinoee <116231263+Arsinoee@users.noreply.github.com>
2022-12-18 21:18:13 -0700

    Create awards.js

---
+++ xmas-template/config_finished_example/awards.js
@@ -0,0 +1,12 @@
+const userAwards ={
+    "certifications": [
+        {
+            "name": "DSU Rising Scholar",
+            "date": "2022"
+        },
+        {
+            "name": "DSU Elite Champion Scholar",
+            "date": "2022"
+        }
+    ],
+}
