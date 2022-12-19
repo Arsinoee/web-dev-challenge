327cd0f739e7cae0f8018422792f077301156b13
Arsinoee <116231263+Arsinoee@users.noreply.github.com>
2022-12-18 21:30:09 -0700

    Create schools.js

---
+++ web-dev-challenge/config/schools.js
@@ -0,0 +1,45 @@
+const userSchools = {
+    "schools": [
+        {
+            "type": "Bachelor's Degree",
+            "name": "Dakota State University",
+            "address": {
+                "street": "820 N Washington St",
+                "city": "Madison",
+                "state": "SD",
+                "zip": "57042"
+            },
+            "major": "Cyber Leadership and Intelligence",
+            "minor": "Military Science (AFROTC)",
+            "gpa": 4.0,
+            "graduation": "May 2026",
+            "clubs": [
+                {
+                    "name": "Newman Club",
+                    "position": "Member"
+                },
+                {
+                    "name": "General Beadle Honors Program",
+                    "position": "Advisory Council Member "
+                },
+                {
+                    "name": "Emry Hall Council",
+                    "position": "Business Admin"
+                },
+            ]
+        },
+        {
+            "type": "High School",
+            "name": "Stevens High School",
+            "address": {
+                "street": "4215 Raider Rd",
+                "city": "Rapid City",
+                "state": "SD",
+                "zip": "57702"
+            },
+            "major": "General Studies",
+            "gpa": 4.1,
+            "graduation": "May 2022"
+        }
+    ]
+}
